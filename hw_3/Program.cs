06b69d5eb4a7254e0e46c9c8a2716564e3508743
Ann <antysya@mail.ru>
2022-05-14 10:39:01 +0300

    Добавьте файлы проекта.

---
+++ hw_3/Program.cs
@@ -0,0 +1,91 @@
+﻿/*Задание 1. 
+Найти в строке все возможные  вложения подстроки. (Рассматривали на уроке)
+
+Задание 2. 
+Доработать класс STUDENT, начатый на уроке. Добавить несколько разных методов для обработки полей класса
+и несколько видов перегруженных операторов ( унарных, бинарных) по собственному усмотрению.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                WriteLine("Выберете действие, которое хотите произвести:\n " +
+                    "1. Найти в строке все вложения подстроки; \n 2. Добавить несколько разных методов " +
+                    "для обработки полей класса и несколько видов перегруженных операторов(унарных, бинарных) по " +
+                    "собственному усмотрению; \n " +
+                    "3. Выйти из программы.");
+                string posnum = ReadLine();
+
+                switch (posnum)
+                {
+                    case "1":
+                        {
+                            WriteLine("Найти в строке все возможные  вложения подстроки.\n " +
+                                "Введите строку, в которой необходимо осуществить поиск:");
+                            string str = ReadLine();
+                            WriteLine("Введите искомую строку: ");
+                            string searchStr = ReadLine();
+
+                            int index = 0;
+                            int cout = 0;
+                            while ((index = str.IndexOf(searchStr, index)) != -1)
+                            {
+                                Write($"{index} ");
+                                index += searchStr.Length;
+                                cout++;
+                            }
+
+                            if (cout == 0)
+                                WriteLine("В строке нет данной подстроки!");
+                            WriteLine();
+                        }
+
+                        break;
+
+                    case "2":
+                        {
+                            WriteLine("Класс Student.");
+                            Student st = new Student("Ann", "Eroshina", 35, 10);
+                            Student st1 = new Student("Oleg", "Eroshin", 10, 11);
+
+                            WriteLine("Список студентов:");
+                            WriteLine(st); 
+                            WriteLine(st1);
+                            WriteLine();
+                            WriteLine($"Средний возраст студентов (через перегрузку +) {(st + st1) / 2}");
+                            WriteLine();
+                            WriteLine("Распечатать отсортированный список самодельным методом Sort с использованием " +
+                                "перегрузки оператора < :");
+                            Student.Sort(st1, st);
+                            WriteLine();
+                            WriteLine($"Средние баллы студентов равны? {st == st1}");
+                            WriteLine($"Средние баллы студентов неравны?{st != st1}");
+                            WriteLine();
+                            Student.Best(st, st1);
+                            WriteLine();
+                        }
+                        break;
+
+                    case "3":
+                        return;
+
+                    default:
+                        WriteLine("Вы выбрали несуществующий пункт меню. Выберите снова.");
+                        continue;
+                }
+            }
+        }
+    }
+}
---
+++ hw_3/Student.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace hw_3
+{
+    class Student
+    {
+        string firstname;
+        string lastname;
+        int age;
+        double avg_bal;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string _firstname, string _lastname, int _age, double _avg_bal)
+        {
+            firstname = _firstname;
+            lastname = _lastname;
+            age = _age;
+            avg_bal = _avg_bal;
+        }
+
+        public void Print()
+        {
+            WriteLine($" {firstname}  {lastname}  {age}  {avg_bal} ");
+        }
+
+        public override string ToString()
+        {
+            return $" {firstname}  {lastname}  {age}  {avg_bal} ";
+        }
+
+        public static int operator +(Student _st1, Student _st2)
+        {
+            return _st1.age + _st2.age;
+        }
+
+        public static bool operator >(Student _st1, Student _st2)
+        {
+            if (_st1.lastname.CompareTo(_st2.lastname) <= 0)
+                return false;
+            else
+                return true;
+        }
+
+        public static bool operator <(Student _st1, Student _st2)
+        {
+            if (_st1.lastname.CompareTo(_st2.lastname) <= 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator ==(Student _st1, Student _st2)
+        { 
+            if (_st1.avg_bal.Equals(_st2.avg_bal))
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Student _st1, Student _st2)
+        {
+            if (_st1.avg_bal.Equals(_st2.avg_bal))
+                return false;
+            else
+                return true;
+        }
+
+        public static void Sort(Student _st1, Student _st2)
+        {
+            if (_st1 < _st2)
+            {
+                _st1.Print();
+                _st2.Print();
+            }
+            else
+            {
+                _st2.Print();
+                _st1.Print();
+            }
+        }
+
+        public static void Best(Student _st1, Student _st2)
+        {
+            if (_st1.avg_bal > _st2.avg_bal)
+                WriteLine($" Лучший студент: {_st1.firstname}  {_st1.lastname} ");
+            else
+                WriteLine($" Лучший студент: {_st2.firstname}  {_st2.lastname} ");
+        }
+    }
+}
+
